1cf2c40540fd2ea7b6cfd2a270b651ca5e2e8ccc
Jeremy <jeremy.hansma@gmail.com>
2022-06-20 15:11:59 +0200

    Push

---
+++ sounds.js
@@ -0,0 +1,22 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0,
+	9,
+	10,
+	12,
+	15,
+	16.5,
+	17,
+	23.3,
+	23.5,
+	24.5,
+	25.8,
+	26.8,
+	31,
+	32,
+	32.8,
+	33,
+	75,
+	86
+];
